b1c9abc7bfb6c21d8b5e10ab040cbbb796617a0d
DaggerFn <143975774+DaggerFn@users.noreply.github.com>
2024-09-09 16:59:07 -0300

    First npm install

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-import { log } from 'node:console'
+/*
 import { createServer } from 'node:http'
 
 const server = createServer((request, response)=>{
   console.log('Ola')
   response.write("Ola Mundo")
+  response.write("oi")
   
   return response.end()
 })
 
 server.listen(3333)
+*/
+
+import { fastify } from "fastify";
+
+const server = fastify()
+
+server.get('/', () =>{
+  return 'Ola mundo'
+})
+
+server.get('/ola', () =>{
+  return 'Ola mundo'
+})
+
+
+server.listen({
+  port: 3333
+})
+
