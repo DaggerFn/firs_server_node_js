1f576b717dda0e30edfcc10bf040d07de2fea58c
DaggerFn <143975774+DaggerFn@users.noreply.github.com>
2024-09-09 14:48:28 -0300

    Update

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-import { createServer } from "node:http";
+// server.mjs
+import { createServer } from 'node:http';
 
-const server = createServer(() => {
-    console.log("Ola")
-})
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
 
-server.listen(3333)
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+
+// run with `node server.mjs`
